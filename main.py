3707d3183ca758ee80c0418c65822672c684e594
Yifan Wu <adamwuqwq@gmail.com>
2022-11-02 18:01:16 +0900

    Updated .gitignore, added VideoWriter

--- main.py
+++ main.py
@@ -30,36 +30,51 @@ if __name__ == "__main__":
     if args.live_demo == True:
         cap = cv2.VideoCapture(0)
     else:
-        cap = cv2.VideoCapture(args.video_input)
+        cap = cv2.VideoCapture(args.video_input, cv2.CAP_FFMPEG)
+        out = cv2.VideoWriter(args.video_output, cv2.VideoWriter_fourcc(*"mp4v"), int(cap.get(cv2.CAP_PROP_FPS)),
+                              (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
-    # Timer for FPS display and elapsed time calculation
+    # For performance evaluation
     prev_frame_time = 0
     new_frame_time = 0
     start_time = time.time()
+    frame_count = 0
+    total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # Loop until the end of video
     while(cap.isOpened()):
+        # Frame count and time count
+        new_frame_time = time.time()
+        frame_count += 1
+
         # Get frame
         ret, frame = cap.read()
+        if ret == False:
+            break
         dets = yolonet.detect(frame)
         srcimg = yolonet.post_process(frame, dets)
-        new_frame_time = time.time()
 
-        # Quit if q is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
+        # Calculate real-time FPS
         fps = 1/(new_frame_time-prev_frame_time)
+        print("frame:", frame_count, "/", total_frame, ", fps =", fps)
 
-
-        if args.live_demo == False:
+        if args.live_demo == True:
             # Show real-time image and FPS
             cv2.putText(frame, str(fps), (10, 40),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
-            cv2.imshow("Face Mosaic - Press q to exit", frame)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
+            cv2.imshow("Face Mosaic Demo", frame)
+        else:
+            # Write current frame to file
+            out.write(frame)
 
         prev_frame_time = new_frame_time
 
-    cv2.waitKey(0)
+    print("Done. Time elapsed:", time.time()-start_time,
+          ", average fps:", total_frame/(time.time()-start_time))
+
     cap.release()
-    cv2.destroyAllWindows()
+    if args.live_demo == True:
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        out.release()
